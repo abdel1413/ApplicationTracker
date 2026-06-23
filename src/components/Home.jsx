4f2986c8adca17d0f82b504d2a7b3af323b57ed6
aboulayetchakoura <aboulayet63@gmail.com>
2026-06-23 11:13:10 -0400

    inititial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Application Tracker</h1>
+      <p>Track your applications efficiently and stay organized.</p>
+    </div>
+  );
+}
