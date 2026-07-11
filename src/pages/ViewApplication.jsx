79c62ef27d540856c16943f05d268b6757a47c51
aboulayetchakoura <aboulayet63@gmail.com>
2026-07-10 23:58:54 -0400

    style status  using terary operator

--- src/pages/ViewApplication.jsx
+++ src/pages/ViewApplication.jsx
@@ -6,7 +6,7 @@ import { Link, useParams } from "react-router-dom"
 export const ViewApplication =()=>{
     const {id} = useParams()
   const [application, setApplication] = useState(null)  
-
+   
   useEffect(()=>{
       const data = JSON.parse(localStorage.getItem('applications'))||[]
   
@@ -30,7 +30,17 @@ export const ViewApplication =()=>{
     <div>
         <h1>{application.company}</h1>
         <p><FaBriefcase /> {application.role}</p>
-        <span>{formattedStatus}</span>
+        <span
+        className={`self-start px-3 py-1 text-sm font-medium"${
+            application.status==="applied"
+        ? "bg-blue-100 text-blue-700"
+        : application.status==='offer'
+        ? 'bg-green-100 text-green-700'
+        : application.status==='interview'
+        ? 'bg-yellow-100 text-yellow-700'
+        : "bg-red-100 text-red-700"}`}
+    
+        >{formattedStatus}</span>
         <div>{dayjs(application.dateApplied).format("MMMM D, YYYY")}</div>
         {application.jobPostingUrl &&(
             <div>
