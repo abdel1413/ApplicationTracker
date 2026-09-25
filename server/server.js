8209fa051745ce12b92caaa152efd38e55a2e3a4
aboulayetchakoura <aboulayet63@gmail.com>
2026-09-25 10:59:12 -0400

    added detete method  to delete a specific app

--- server/server.js
+++ server/server.js
@@ -101,10 +101,25 @@ app.post("/api/applications",  async (req, res) => {
 app.delete("/api/applications/:id", async (req, res) => {
     const {id} = req.params; 
     console.log(id)
-    res.json({message: "delete route reached",
-        id: id
-    })
-})
+    // res.json({message: "delete route reached",
+    //     id: id
+    // })
+    try {
+         const result = await pool.query('DELETE FROM applications WHERE id=$1 RETURNING id ,company, job_posting_url AS "jobPostingUrl", role, date_applied as "dateApplied", status, notes,created_at as "createdAt"', [id])
+ 
+  if(result.rows.length === 0) {
+        return res.status(404).json({ error: 'Application not found' });
+    }
+    res.status(200).json({ message: 'Application deleted successfully', application: result.rows[0] });
+
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Failed to delete application' });
+    }
+    
+ 
+});
 
 const PORT = 5001;
 app.listen(PORT, () => {
